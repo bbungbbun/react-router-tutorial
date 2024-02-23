3fbe43ed493027cce083ee475b4f69fc7ab033a9
bbungbbun <tnqkrdbsl@naver.com>
2024-02-23 23:04:52 +0900

    the root route

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>home</h1>,
-  },
-  {
-    path: "/app",
-    element: <App/>,
-  },
-  {
-    path: "/sub",
-    element: <h1>sub</h1>,
+    element: <Root />,
   },
 ]);
 
