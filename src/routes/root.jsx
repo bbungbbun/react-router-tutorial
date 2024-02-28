42327d5dbeef4ae10900fb590d7df32455ea1f8b
bbungbbun <tnqkrdbsl@naver.com>
2024-02-28 17:37:33 +0900

    active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,6 @@
 import {
   Outlet,
-  Link,
+  NavLink,
   useLoaderData,
   Form,
   redirect,
@@ -70,8 +70,17 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
+                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
                       </>
@@ -79,7 +88,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
