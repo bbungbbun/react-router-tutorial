f06408ef7e7ee1152f844be2c19a2ede4ef6e9cf
bbungbbun <tnqkrdbsl@naver.com>
2024-02-28 17:30:12 +0900

    mutation discussion

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -7,7 +7,10 @@ import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
     const formData = await request.formData();
+    // const firstName = formData.get("first");
+    // const lastName = formData.get("last");
     const updates = Object.fromEntries(formData);
+    console.log("updates", updates)
     await updateContact(params.contactId, updates);
     return redirect(`/contacts/${params.contactId}`);
 }
